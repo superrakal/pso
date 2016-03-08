3b47794b715e1b3f1759ab12d4208b37391f14d3
Egor <topolnyak012@gmail.com>
2016-03-08 17:18:28 +0600

    admin panel

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,9 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  config.before_configuration do
+    ENV['site_url'] = 'pso.kz'
+  end
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
@@ -40,6 +43,9 @@ Rails.application.configure do
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+config.before_configuration do
+    ENV['site_url'] = 'suncity.kerweb.ru'
+  end
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
